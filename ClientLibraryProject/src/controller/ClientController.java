cc975007239f06fe5f7ba49cec4a9b77e96fd728
Jeimmy Cupa Martinez <jeimmy.cupa456@gmail.com>
2022-12-03 17:27:38 -0500

    Cambio client

--- ClientLibraryProject/src/controller/ClientController.java
+++ ClientLibraryProject/src/controller/ClientController.java
@@ -14,7 +14,7 @@ import view.MainWindow;
 public class ClientController implements ActionListener{
 	private MainWindow window;
 	public ClientController() {
-		
+		System.out.println("Prueba");
 		window = new MainWindow(this,obtainBookSet());
 		window.setVisible(true);
 	}
