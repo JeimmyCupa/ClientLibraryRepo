2ed4e74b0f6a170d05c956d6f7bfdc9f1a834120
JuanArchila13 <juanpabloarchila2003@outlook.com>
2022-12-06 16:39:25 -0500

    Importacion de las librerias

--- ClientLibraryProject/src/controller/Net.java
+++ ClientLibraryProject/src/controller/Net.java
@@ -1,46 +1,37 @@
 package controller;
 
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import com.google.gson.Gson;
 
 public class Net {
-    private Socket socket;
-    private DataInputStream input;
-    private DataOutputStream output;
-    public Net(Socket socket) throws IOException {
-        this.socket = socket;
-        this.input = new DataInputStream(socket.getInputStream());
-        this.output = new DataOutputStream(socket.getOutputStream());
-        
-    }
-    public Socket getSocket() {
-        return socket;
-    }
-    public void setSocket(Socket socket) {
-        this.socket = socket;
-    }
-    public DataInputStream getInput() {
-        return input;
-    }
-    public void setInput(DataInputStream input) {
-        this.input = input;
-    }
-    public DataOutputStream getOutput() {
-        return output;
-    }
-    public void setOutput(DataOutputStream output) {
-        this.output = output;
-    }
-    public void close() {
-        try {
-            this.socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    
+	private Socket client;
+	private DataInputStream input;
+	private DataOutputStream output;
+	private Gson myGson;
+	
+	public Net(Socket client) throws IOException{
+		this.myGson = new Gson();
+		this.client = client;
+		this.input = new DataInputStream(this.client.getInputStream());
+		this.output = new DataOutputStream(this.client.getOutputStream());
+	}
+	
+	public Socket getClient() {
+		return client;
+	}
+
+	public DataInputStream getInput() {
+		return input;
+	}
+
+	public DataOutputStream getOutput() {
+		return output;
+	}
+	
+	public Gson getMyGson() {
+		return myGson;
+	}
 }
